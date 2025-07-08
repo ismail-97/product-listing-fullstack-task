0e6deb4fcd4b35e502206742bf8270cc612003c2
ISMDEW <ismailkamaleldin@gmail.com>
2025-07-08 12:58:33 +0300

    handle env variables- prepare for deployment

--- interface/src/services/products.ts
+++ interface/src/services/products.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import type { ProductEntry } from '../types';
+const apiUrl = import.meta.env.VITE_API_URL;
+
 
 export const getProducts = async (): Promise<ProductEntry[]> => {
-    const response = await axios.get<ProductEntry[]>("/api/products");
+    const response = await axios.get<ProductEntry[]>(`${apiUrl}api/products`);
     return response.data;
 }
